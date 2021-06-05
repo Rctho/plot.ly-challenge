4d9c2c22099dafa8ac6367dbadb708ad8b94db82
Rachel T <rachelceline15@gmail.com>
2021-06-05 13:22:58 -0500

    edits to js

--- static/js/app.js
+++ static/js/app.js
@@ -7,15 +7,12 @@ function optionChanged(selectedID){
     // Read the json file for the data
     d3.json("./data/samples.json").then((data) => {
  
-   //  console.log(data);
- 
     // Clears dropdown
     d3.select("#selDataset").html("");   
     
     // Select the metadata array and for each item append the item ID and adds ID to dropdown
     data.metadata.forEach(item =>
          {
-          // console.log(item.id);
          d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
          });
     // Selected value is passed
@@ -23,12 +20,7 @@ function optionChanged(selectedID){
     
     // Filter Metadata for selected ID from dropdown
     const idMetadata = data.metadata.filter(item=> (item.id == selectedID));
-       // {
-       //    console.log("------------------------")
-       //    console.log(item);
-       //    console.log(item.id);
-          
-       // });
+
     // Check the metadata loaded for the selected ID
     console.log(idMetadata);
     
@@ -36,20 +28,15 @@ function optionChanged(selectedID){
     panelDisplay.html("");
     Object.entries(idMetadata[0]).forEach(item=> 
        {
-          // console.log(item);
           panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
        });
  
     // BAR CHART
+    // modified from https://github.com/amagsino/Plot.ly-Challenge
  
     // Filter sample array data for the selected ID
     const idSample = data.samples.filter(item => parseInt(item.id) == selectedID);
     
-    // // Check values
-    // console.log(typeof parseInt(item.id));
-    // console.log(idSample[0].sample_values);  
-    // console.log(idSample[0].otu_ids);  
-    // console.log(idSample[0].otu_labels);  
     
     // Slice top 10 sample values
     var sampleValue = idSample[0].sample_values.slice(0,10);
@@ -59,14 +46,8 @@ function optionChanged(selectedID){
     var otuLabels = idSample[0].otu_labels
     otuLabels = otuLabels.reverse();
  
-    // // Check values
-    //  console.log(sampleValue);
-    //  console.log(otuID);
-    //  console.log(otuLabels);
- 
     // Y axis of bar chart
     const yAxis = otuID.map(item => 'OTU' + " " + item);
-       // console.log(yAxis);
     
     // Define the layout and trace object, edit color and orientation
        const trace = {
@@ -76,14 +57,11 @@ function optionChanged(selectedID){
        orientation: "h",
        text:  otuLabels,
        marker: {
-          color: 'rgb(154, 140, 152)',
-          line: {
-             width: 3
-         }
+          color: 'blue',
         }
        },
        layout = {
-       title: 'Top 10 Operational Taxonomic Units (OTU)/Individual',
+       title: 'Top 10 Bacteria Cultures Found',
        xaxis: {title: 'Number of Samples Collected'},
        yaxis: {title: 'OTU ID'}
        };
@@ -110,12 +88,10 @@ function optionChanged(selectedID){
   },
  
   layout1 = {
-    title: '<b>Bubble Chart For Each Sample</b>',
+    title: '<b>Bacteria Cultures Per Sample</b>',
     xaxis: {title: 'OTU ID'},
     yaxis: {title: 'Number of Samples Collected'},
     showlegend: false,
-    height: 800,
-    width: 1800
     };
     
  // Plot using Plotly
